b10739c36f02bb3c8c92d1e37ec732246b67df40
Thecrazyskull <anaskarbila@gmail.com>
2017-01-25 12:00:05 -0600

    PhotoModule: update focus UI on UI thread
    Change-Id: I37681f99c8bd9feda2af537d210bbd410e1384bb

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1318,7 +1318,9 @@ public class PhotoModule
             Log.v(TAG, "mPictureDisplayedToJpegCallbackTime = "
                     + mPictureDisplayedToJpegCallbackTime + "ms");
 
-            mFocusManager.updateFocusUI(); // Ensure focus indicator is hidden.
+            mHandler.post(() -> {
+                mFocusManager.updateFocusUI(); // Ensure focus indicator is hidden.
+            });
 
             boolean needRestartPreview = !mIsImageCaptureIntent
                     && !mPreviewRestartSupport
