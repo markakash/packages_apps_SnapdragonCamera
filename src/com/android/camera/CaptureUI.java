806fad2a268a32642a3beb20662bdca2e70d30a7
Lennart Wieboldt <lennart.1997@gmx.de>
2017-09-20 10:32:18 +0200

    ParanoidCamera: replace the panorama button
    Use the photo shutter icon to be more consistent.
    The old icon doesn´t fit the modern look of the app
    Also use "real" white for the stop recording button
    
    Change-Id: I0ffaeccedb3b7c28a340971beb06e1099e1fb9e7
    Signed-off-by: Lennart Wieboldt <lennart.1997@gmx.de>
    Signed-off-by: Alex Naidis <alex.naidis@paranoidandroid.co>

--- src/com/android/camera/CaptureUI.java
+++ src/com/android/camera/CaptureUI.java
@@ -864,7 +864,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
             } else {
                 mFlashButton.init(true);
             }
-            mVideoButton.setImageResource(R.drawable.video_stop);
+            mVideoButton.setImageResource(R.drawable.shutter_button_video_stop);
             mRecordingTimeView.setText("00:00");
             mRecordingTimeRect.setVisibility(View.VISIBLE);
             mMuteButton.setVisibility(View.VISIBLE);
